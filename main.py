633e90d1f1cc9bbc87e3c439b47bcac0c0dc31e5
gobbls <98641699+gobbls@users.noreply.github.com>
2026-01-28 11:39:27 +0100

    propper logging

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 
-from codec.File import File
+from codec.file import File
 import codec.constants as c
 
 import os
 import subprocess
+import logging
 from sys import argv
 from pathlib import Path
 
 
-target_path = argv[1]
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+target_paths = argv[1:]
 
 
 def check_dependencies() -> None:
@@ -18,23 +23,26 @@ def check_dependencies() -> None:
     try:
         subprocess.run(['ffprobe', '-loglevel', 'quiet'])
     except FileNotFoundError:
-        raise ValueError('[ERROR] ffprobe not found!')
+        logger.error('Missing dependency "FFprobe"!')
+        raise FileNotFoundError('[ERROR] FFprobe not found!')
 
-    # Check that the given path exists
-    if not os.path.exists(target_path):
-        raise ValueError('[ERROR] The given target does not exist!')
-
-    # Check that the given target is a directory
-    if not Path.is_dir(target_path):
-        raise ValueError('[ERROR] The given target is not a directory!')
-
-    print("[LOG] Dependencies OK")
+    logger.info('Dependencies OK')
 
 
-def parse_args() -> None:
-    if len(argv) > 2 or len(argv) == 1:
-        raise ValueError('[ERROR] Currently only supports a single directory!')
-    print("[LOG] Parsed OK")
+def check_targets() -> None:
+    # Check if the given target(s) exists
+    for path in target_paths:
+        if not os.path.exists(path):
+            logger.error('One or more of the given targets does not exist!')
+            raise FileNotFoundError('[ERROR] The given target(s) does not exist!')
+
+    # Check if the given target(s) are directories
+    for path in target_paths:
+        if not Path.is_dir(path):
+            logger.error('One or more of the given targets are not directories!')
+            raise NotADirectoryError('[ERROR] The given target is not a directory!')
+
+    logger.info('Target directories OK')
 
 
 def get_videos_from_path(path: str) -> [Path]:
@@ -42,20 +50,29 @@ def get_videos_from_path(path: str) -> [Path]:
 
 
 def main():
-    paths: [Path] = get_videos_from_path(target_path)
-    print("[LOG] Got videos from target")
-    for file in paths:
-        print("\t>", file)
-    files: [File] = [File(f) for f in paths]
-    print("[LOG] Initialized File object for every target files")
+    paths: [Path] = []
 
+    for target in target_paths:
+        paths.extend(get_videos_from_path(target))
+
+    logger.info('Got videos from target:')
+    for path in paths:
+        print("  >", path)
+
+    files: [File] = [File(f) for f in paths]
+
+    logger.info("Initialized File object for every target files")
+
+    logger.info("Info:")
+    separator = "-" * (os.get_terminal_size().columns - 1)
     for file in files:
+        print(separator)
         print("[>] Video path:", file.video_path)
-        print("[>] Video audio codec:", file.audio_codec)
-        print("[>] Generated image path:", file.image_path)
+        print("  [>] Video audio codec:", file.audio_codec)
+        print("  [>] Generated image path:", file.image_path)
 
 
 if __name__ == '__main__':
 	check_dependencies()
-	parse_args()
+	check_targets()
 	main()
